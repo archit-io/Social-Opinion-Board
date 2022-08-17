ea528cddace1d5fc6c9fdf4a4a977bcf4f995cfb
Archit Choudhary <archit.digisocial@gmail.com>
2022-08-17 17:43:19 +0530

    Added helpers

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-//@desc Login/landing page
-//@route GET /
-router.get('/', (req,res) => {
-    res.render('Login', {
-        layout: 'login'
-    })
+const Story = require('../models/Story')
+
+// @desc    Login/Landing page
+// @route   GET /
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
 })
 
-//@desc Dashboard
-//@route GET /dashboard
-router.get('/dashboard', (req,res) => {
-    res.render('Dashboard')
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 module.exports = router
