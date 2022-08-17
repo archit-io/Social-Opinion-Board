e73a39fb8fbf919a4dc3ef2693ab457bf84f2d38
Archit Choudhary <archit.digisocial@gmail.com>
2022-08-17 12:30:39 +0530

    added morgan middleware in app.js for logging

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ connectDB()
 
 const app = express()
 
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  }
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT,
